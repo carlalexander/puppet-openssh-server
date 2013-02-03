2cc60ec87692a2c546db99ac62636770a0dfdc11
Carl Alexander <carlalexander@gmail.com>
2013-02-03 13:23:22 -0500

    Added some more options

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,4 +2,8 @@ class openssh_server::params {
   $port              = 22
   $permit_root_login = 'no'
   $server_key_bits   = '768'
+
+  $ssh_host_rsa_key   = '/etc/ssh/ssh_host_rsa_key'
+  $ssh_host_dsa_key   = '/etc/ssh/ssh_host_dsa_key'
+  $ssh_host_ecdsa_key = '/etc/ssh/ssh_host_ecdsa_key'
 }
